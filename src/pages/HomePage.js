dfde06de2236c6c3b01c23539fe070a7185a498e
forwardtop <noproblemgod@hotmail.com>
2024-01-14 17:42:34 +0800

    homepage works

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function HomePage() {
+    return (
+      <div>
+        <h1 className='text-black'>this is homepage</h1>
+      </div>
+    );
+  }
+
+ export default HomePage; 
+  
+  
